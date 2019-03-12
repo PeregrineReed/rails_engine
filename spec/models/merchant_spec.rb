6ca942a52468409f91db15e141afd64eb476dbb1
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-12 14:21:29 -0600

    Added relationships and validations for merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  
+
+  describe 'Relationships' do
+    it { should have_many :items }
+    it { should have_many :invoices }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'Class Methods' do
+  end
+
+  describe 'Instance Methods' do
+  end
 end
