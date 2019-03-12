1fd6e3953124c47d96e11fc30d0dbdfb5084282c
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-12 11:35:57 -0600

    added validations for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  
+
+  describe 'Relationships' do
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+  end
+
+  describe 'Class Methods' do
+  end
+
+  describe 'Instance Methods' do
+  end
+
 end
