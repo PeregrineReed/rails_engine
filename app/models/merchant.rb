c9414a64bc114e83dcf7cd022f5be1301c6ed734
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-13 23:26:24 -0600

    Updated models: merchant(revenue by day), invoice, transaction

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+
+  def revenue_by_date(date)
+    invoices.select("sum(unit_price*quantity) AS revenue").joins(:invoice_items, :transactions).merge(Transaction.successful).where(updated_at: Date.parse(date).all_day)[0]
+  end
 end
