965f47aa558e335dad26559f24700942f462e00f
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-13 23:25:30 -0600

    Added tests for revenue by day for a merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -15,5 +15,13 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe 'Instance Methods' do
+    it ".revenue_by_date(date)" do
+      merchant = create(:merchant)
+      invoice = create(:invoice, merchant: merchant, updated_at: "2012-03-25 09:54:09 UTC")
+      create(:invoice_item, invoice: invoice, quantity: 5, unit_price: 5000)
+      create(:transaction, result: 'success', invoice: invoice)
+
+      expect(merchant.revenue_by_date("2012-03-25").revenue).to eq(25000)
+    end
   end
 end
