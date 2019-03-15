2963c5f81cb4cc599520f804a4b2c0eb7c118fd3
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-15 10:36:20 -0600

    Created spec for highest revenue items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,6 +13,39 @@ RSpec.describe Item, type: :model do
   end
 
   describe 'Class Methods' do
+
+    it '::highest_revenue(limit)' do
+      items = create_list(:item, 5)
+      counter = 1
+      items.each do |item|
+        invoice = create(:invoice)
+        create(:transaction, invoice: invoice, result: 'success')
+        counter.times do
+          create(:invoice_item, item: item, invoice: invoice, unit_price: 1000 * counter, quantity: 10 * counter)
+        end
+        counter += 1
+      end
+      invoice = create(:invoice)
+      create(:transaction, invoice: invoice, result: 'success')
+      create(:invoice_item, item: items[2], invoice: invoice, unit_price: 5000 * counter, quantity: 25 * counter)
+
+      three_items = Item.highest_revenue(3)
+      two_items = Item.highest_revenue(2)
+      one_item = Item.highest_revenue(1)
+
+      expect(three_items.length).to eq(3)
+      expect(three_items[0]).to eq(items[2])
+      expect(three_items[1]).to eq(items[4])
+      expect(three_items[2]).to eq(items[3])
+
+      expect(two_items.length).to eq(2)
+      expect(two_items[0]).to eq(items[2])
+      expect(two_items[1]).to eq(items[4])
+
+      expect(one_item.length).to eq(1)
+      expect(one_item[0]).to eq(items[2])
+    end
+
   end
 
   describe 'Instance Methods' do
