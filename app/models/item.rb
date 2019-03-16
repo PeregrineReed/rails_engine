57a3675b6dc1059eaa72aa9c2c1b3246a6391a7d
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-16 16:52:42 -0600

    Created ::best_day(id) method for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,4 +23,13 @@ class Item < ApplicationRecord
     .limit(limit)
   end
 
+  def self.best_day(id)
+    select("items.id, CAST(invoices.updated_at AS DATE) as day, SUM(invoice_items.quantity) AS sales")
+    .joins(invoice_items: {invoice: :transactions})
+    .merge(Transaction.successful)
+    .where('items.id = ?', id)
+    .group('invoices.updated_at, items.id')
+    .order('sales DESC, day DESC')[0]
+  end
+
 end
