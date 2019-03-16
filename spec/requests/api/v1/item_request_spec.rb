2ac158f38e0418627cbeefdc9d94ba2ab006bd80
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-15 22:48:47 -0600

    Added spec for Item.most_sold API call to /items/most_items/quantity

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -9,14 +9,12 @@ RSpec.describe 'Item API' do
     items.each do |item|
       invoice = create(:invoice)
       create(:transaction, invoice: invoice, result: 'success')
-      counter.times do
-        create(:invoice_item, item: item, invoice: invoice, unit_price: 1000 * counter, quantity: 10 * counter)
-      end
+      create(:invoice_item, item: item, invoice: invoice, unit_price: 1000 * counter, quantity: 10)
       counter += 1
     end
     invoice = create(:invoice)
     create(:transaction, invoice: invoice, result: 'success')
-    create(:invoice_item, item: items[2], invoice: invoice, unit_price: 5000 * counter, quantity: 25 * counter)
+    create(:invoice_item, item: items[2], invoice: invoice, unit_price: 5000 * counter, quantity: 1)
 
     get "/api/v1/items/most_revenue?quantity=3"
     json = JSON.parse(response.body)
@@ -34,6 +32,41 @@ RSpec.describe 'Item API' do
     expect(json["data"][1]["attributes"]["id"].to_i).to eq(items[4].id)
 
     get "/api/v1/items/most_revenue?quantity=1"
+    json = JSON.parse(response.body)
+
+    expect(json["data"].length).to eq(1)
+    expect(json["data"][0]["attributes"]["id"].to_i).to eq(items[2].id)
+  end
+
+  it 'returns the requested number of items that have sold the most by quantity' do
+    items = create_list(:item, 5)
+    counter = 1
+    items.each do |item|
+      invoice = create(:invoice)
+      create(:transaction, invoice: invoice, result: 'success')
+      create(:invoice_item, item: item, invoice: invoice, unit_price: 1000, quantity: 10 * counter)
+      counter += 1
+    end
+    invoice = create(:invoice)
+    create(:transaction, invoice: invoice, result: 'success')
+    create(:invoice_item, item: items[2], invoice: invoice, unit_price: 100, quantity: 25 * counter)
+
+    get "/api/v1/items/most_items?quantity=3"
+    json = JSON.parse(response.body)
+
+    expect(json["data"].length).to eq(3)
+    expect(json["data"][0]["attributes"]["id"].to_i).to eq(items[2].id)
+    expect(json["data"][1]["attributes"]["id"].to_i).to eq(items[4].id)
+    expect(json["data"][2]["attributes"]["id"].to_i).to eq(items[3].id)
+
+    get "/api/v1/items/most_items?quantity=2"
+    json = JSON.parse(response.body)
+
+    expect(json["data"].length).to eq(2)
+    expect(json["data"][0]["attributes"]["id"].to_i).to eq(items[2].id)
+    expect(json["data"][1]["attributes"]["id"].to_i).to eq(items[4].id)
+
+    get "/api/v1/items/most_items?quantity=1"
     json = JSON.parse(response.body)
 
     expect(json["data"].length).to eq(1)
