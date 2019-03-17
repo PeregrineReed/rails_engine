92677e70f9bcc96d28cc6b204082855438922bdb
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-17 11:57:19 -0600

    Controllers for invoice API

--- app/controllers/api/v1/items/most_revenue/quantity_controller.rb
+++ app/controllers/api/v1/items/most_revenue/quantity_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Items::MostRevenue::QuantityController < ApplicationController
 
   def index
-    render json: TopItemsSerializer.new(Item.highest_revenue(params[:quantity]))
+    render json: ItemSerializer.new(Item.highest_revenue(params[:quantity]))
   end
 
 end
