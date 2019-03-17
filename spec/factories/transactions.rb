9d2a478438e02ec3b011393d604f6054a63f0c55
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-17 14:48:26 -0600

    Updates to factories and serializers

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :transaction do
     invoice
-    credit_card_number { 1 }
+    credit_card_number { "1000100010001000" }
+    credit_card_expiration_date { "2012-12-21" }
     result { 0 }
   end
 end
