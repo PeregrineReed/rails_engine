f0a74f786e13497d9e9a92f7989095c8ee234ef8
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-16 18:01:54 -0600

    Added spec for merchant index and show pages

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -2,6 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
 
+  it 'sends a list of all merchants' do
+    merchants = create_list(:merchant, 5)
+
+    get "/api/v1/merchants"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)
+
+    expect(json["data"].count).to eq(5)
+  end
+
+  it 'sends a single merchant' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)
+
+    expect(json["data"]["id"]).to eq(id.to_s)
+  end
+
   it 'returns defined number of merchants ordered by most revenue' do
     merchants = create_list(:merchant, 4)
     invoices = []
